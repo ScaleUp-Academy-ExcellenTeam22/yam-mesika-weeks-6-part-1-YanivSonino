1b18dbcc04c046104913f9b93d49d4eb38556dba
Janiv <y1a2n3i4v55@gmail.com>
2022-04-25 13:47:20 +0300

    Documentation added, Simplfied the full names function

--- Those_are_the_names.py
+++ Those_are_the_names.py
@@ -1,9 +1,15 @@
+
 def full_names(first_name_list, last_name_list, min_len=0):
-    return \
-        [first_name.capitalize() + ' ' + last_name.capitalize()
-         for first_name in first_name_list
-         for last_name in last_name_list
-         if len(first_name + ' ' + last_name) >= min_len]
+    """
+    Combine first and last name and returns the ones that have pass the minimum length.
+
+    :param first_name_list: The list of the first names.
+    :param last_name_list: The list of the last names.
+    :param min_len: The minimum length of the full name.
+    :return: List of all the full names combination that pass the minimum length.
+    """
+    full_names_list = [first_name.capitalize() + ' ' + last_name.capitalize() for first_name in first_name_list for last_name in last_name_list]
+    return list(filter(lambda full_name: len(full_name) >= min_len, full_names_list))
 
 
 if __name__ == '__main__':
