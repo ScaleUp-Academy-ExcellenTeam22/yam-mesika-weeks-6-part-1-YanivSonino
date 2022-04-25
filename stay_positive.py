1e190078d260dffc1da18a91e1bec353f7f50a17
Janiv <y1a2n3i4v55@gmail.com>
2022-04-25 14:04:44 +0300

    Documentation added, simplified the function

--- stay_positive.py
+++ stay_positive.py
@@ -1,9 +1,12 @@
 def get_positive_numbers():
-    return tuple(
-        filter(lambda n: n > 0,  # filter positive numbers
-               map(lambda n: int(n),  # casting all elements in list to int
-                   input("enter list of numbers seperate by ',': ").split(
-                       ','))))  # gets string from the user and create a list seperate by ','
+    """
+    Gets list of numbers from the user and finds the positive numbers.
+
+    :return: Tuple of positive numbers.
+    """
+    list_of_numbers = input("enter list of numbers seperate by ',': ").split(',')  # gets string from the user
+    return tuple(filter(lambda n: n > 0, map(lambda number: int(number), list_of_numbers)))
 
 
-print(get_positive_numbers())
+if __name__ == '__main__':
+    print(get_positive_numbers())
