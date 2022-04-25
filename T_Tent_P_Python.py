de4feb7fd5a14a9bf5cae7b3147a5a8ff568a213
Janiv <y1a2n3i4v55@gmail.com>
2022-04-25 14:20:08 +0300

    Documentation added, spaceing between operators.

--- T_Tent_P_Python.py
+++ T_Tent_P_Python.py
@@ -1,7 +1,11 @@
 def get_letter():
-    return [chr(letter) for letter in range(ord('A'), ord('z')+1) if letter <= ord('Z') or letter >= ord('a')]
+    """
+    Get all the letters in the English Alphabet.
+
+    :return: List of the English Alphabet.
+    """
+    return [chr(letter) for letter in range(ord('A'), ord('z') + 1) if letter <= ord('Z') or letter >= ord('a')]
 
 
 if __name__ == '__main__':
-    letter_list = get_letter()
-    print(letter_list)
+    print(get_letter())
