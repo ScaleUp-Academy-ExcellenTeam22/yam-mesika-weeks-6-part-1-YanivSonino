4cfaefc158a2bdaa197276ba11a1505c353074ce
Janiv <y1a2n3i4v55@gmail.com>
2022-04-25 14:12:08 +0300

    Documentation added.

--- running_2000.py
+++ running_2000.py
@@ -1,12 +1,21 @@
 import time
 
 
-def timer(f, *structure, **structure_keyword):
+def timer(function, *structure, **structure_keyword):
+    """
+    Count the time that the function took to work over the structure.
+
+    :param function: The function we want to measure.
+    :param structure: The iterable structure we want to check.
+    :param structure_keyword: The key structure we want to check.
+    :return: The time took to finish the function.
+    """
     start_time = time.time()
-    map(f, filter(None, [structure, structure_keyword]))
+    map(function, filter(None, [structure, structure_keyword]))
     return time.time() - start_time
 
 
-print(timer(print, "Hello"))
-print(timer(zip, [1, 2, 3], [4, 5, 6]))
-print(timer("Hi {name}".format, name="Bug"))
+if __name__ == '__main__':
+    print(timer(print, "Hello"))
+    print(timer(zip, [1, 2, 3], [4, 5, 6]))
+    print(timer("Hi {name}".format, name="Bug"))
