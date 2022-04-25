0e48dcc3d42cc2a5f12b1e0acfe1dd1f1138bd9f
Janiv <y1a2n3i4v55@gmail.com>
2022-04-25 13:52:33 +0300

    Documentation added, Simplfied the loop

--- custom_filter.py
+++ custom_filter.py
@@ -1,6 +1,12 @@
-def my_filter(func, structure):
-    lst = []
-    for i in structure:
-        if func(i):
-            lst.append(i)
-    return lst
+def my_filter(function, structure):
+    """
+
+    :param function: The function you want to apply on every item in the structure
+    :param structure: The structure you want to apply the filter
+    :return: list of filtered structure
+    """
+    return [item for item in structure if function(item)]
+
+
+if __name__ == '__main__':
+    print(my_filter(lambda x: x > 3, range(1, 10)))
